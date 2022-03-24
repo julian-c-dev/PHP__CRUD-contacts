8247a9909fc4bdd053dcb297179601012e70553d
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-03-24 15:07:05 +0000

    Partials html

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $host = "localhost";
 $database = "contacts_app";
@@ -6,11 +6,11 @@ $user = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-    // foreach ($conn->query("SHOW DATABASES") as $row) {
-    //     print_r($row);
-    // }
-    // die();
+  $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+  // foreach ($conn->query("SHOW DATABASES") as $row) {
+  //   print_r($row);
+  // }
+  // die();
 } catch (PDOException $e) {
-    die("PDO Connection Error: " . $e->getMessage()); 
+  die("PDO Connection Error: " . $e->getMessage());
 }
